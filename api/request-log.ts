5a488e4717aa2721cebd29ea4fc4ee3b39f82917
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-12-18 09:26:49 -0800

    types + comments

--- api/request-log.ts
+++ api/request-log.ts
@@ -1,6 +1,22 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+import { NowRequest, NowRequestCookies, NowRequestQuery, NowResponse } from '@vercel/node'
+import { UserData } from 'auth0';
+import { IncomingHttpHeaders } from 'http2';
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+
+type LogEntry = {
+  statusCode: number
+  tenant: string
+  context: Record<string, unknown>
+  user: UserData,
+  ruleIp: string | string[]
+  ruleUserAgent: string
+  request: {
+    "headers": IncomingHttpHeaders
+    "query": NowRequestQuery
+    "cookies": NowRequestCookies
+  }
+}
 
 const LogEntry = new Schema({
   date: { type: Date, default: Date.now },
@@ -17,34 +33,35 @@ const LogEntry = new Schema({
 const Log = mongoose.model('LogEntry', LogEntry);
 
 export default async (request: NowRequest, response: NowResponse) => {
+  // TODO: validate body
   const { tenant = 'unknown', context, user } = JSON.parse(request.body)
 
-  const logResponse = {
-    "statusCode": response.statusCode,
+  const logResponse: LogEntry = {
+    statusCode: response.statusCode,
     tenant,
     context,
     user,
-    "ruleIp": request.headers["x-real-ip"],
-    "ruleUserAgent": request.headers["user-agent"],
-    "request": {
-      "headers": request.headers,
-      "query": request.query,
-      "cookies": request.cookies
+    ruleIp: request.headers["x-real-ip"],
+    ruleUserAgent: request.headers["user-agent"],
+    request: {
+      headers: request.headers,
+      query: request.query,
+      cookies: request.cookies
     }
   }
+
+  // TODO: Catch errors
+  await mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+
+  const newLogEntry: mongoose.Document<LogEntry> = new Log(logResponse);
   
-  // TODO: Place this logReponse a database
-
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    })
-
-  const newLogEntry = new Log(logResponse);
-
-  newLogEntry.save({checkKeys: false}, function (err) {
+  // TODO: Catch errors
+  newLogEntry.save({checkKeys: false}, err => {
     if (err) return console.error(err);
     response.status(200).send(logResponse)
   })
